9cd49e51bcf51ba3f2d900f0c94ad4eeddc1ecd4
gtesei <Gino.Tesei@anthem.com>
2020-12-16 12:30:59 -0800

    ...

--- test.py
+++ test.py
@@ -43,8 +43,13 @@ def main(args):
 
     # Get model
     log.info('Building model...')
-    model = BiDAF(word_vectors=word_vectors,
-                  hidden_size=args.hidden_size)
+    if args.name == 'baseline_char_embed':
+        model = BiDAF_charCNN(word_vectors=word_vectors,
+                              char_vectors=char_vectors,
+                              hidden_size=args.hidden_size) 
+    else:
+        model = BiDAF(word_vectors=word_vectors,
+                      hidden_size=args.hidden_size)
     model = nn.DataParallel(model, gpu_ids)
     log.info(f'Loading checkpoint from {args.load_path}...')
     model = util.load_model(model, args.load_path, gpu_ids, return_step=False)
@@ -78,7 +83,10 @@ def main(args):
             batch_size = cw_idxs.size(0)
 
             # Forward
-            log_p1, log_p2 = model(cw_idxs, qw_idxs)
+            if args.name == 'baseline_char_embed':
+                log_p1, log_p2 = model(cw_idxs, cc_idxs, qw_idxs, qc_idxs) 
+            else:
+                log_p1, log_p2 = model(cw_idxs, qw_idxs)
             y1, y2 = y1.to(device), y2.to(device)
             loss = F.nll_loss(log_p1, y1) + F.nll_loss(log_p2, y2)
             nll_meter.update(loss.item(), batch_size)
